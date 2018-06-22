adb71621e4cc1b7f5404e5352b2c9ba6bb94efbb
ZIN KO KO MIN <zkkmin@gmail.com>
2018-06-22 02:31:57 +0000

    Add tests

--- ClientLoc/tests.py
+++ ClientLoc/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.test import Client
+from . import views
 
 # Create your tests here.
+class TestView(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+    
+    def test_get_country(self):
+        request = self.factory.get('/country/')
+        # request.META.get('REMOTE_ADDR')
+        request.META['REMOTE_ADDR'] = "103.224.165.57"
+        
+        response = views.get_country(request)
+        self.assertEqual(response.status_code, 200)
